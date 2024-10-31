d7052327bd337b7f116742bf5a3071b12e0ae1d0
Kainatshafq <imkainatshafiqofficial523@gmail.com>
2024-10-31 19:58:29 +0500

    add my repo

---
+++ task4(for).cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1;i<=3;i++)
+    {
+        cout<<"weeks"<<i<<endl;
+        for(int j=1;j<=7;j++)
+        {
+            cout<<"day"<<j<<endl;
+        }
+    }
+
+}
